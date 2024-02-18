64484c9466ebd5cf11e8de0fc13029847d355acc
Ankit Garg <ankit.garg0104@gmail.com>
2024-02-18 23:22:21 +0530

    commit

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,17 +1,36 @@
+def myMethod() {
+    // Your method implementation here
+    // Should return a list of strings representing the options
+    return ['Option1', 'Option2', 'Option3']
+}
+
+def options = myMethod()
+
+properties([
+    parameters([
+        [$class: 'ChoiceParameter',
+         choiceType: 'PT_SINGLE_SELECT',
+         description: 'Select an option',
+         filterLength: 1,
+         name: 'SelectedOption',
+         script: [
+             $class: 'GroovyScript',
+             fallbackScript: [
+                 classpath: [],
+                 sandbox: false,
+                 script: 'return ["Error"]'
+             ],
+             script: [
+                 classpath: [],
+                 sandbox: false,
+                 script: "return ${options.inspect()}"
+             ]
+         ]]
+    ])
+])
+
 pipeline {
     agent any
-
-    parameters {
-        string(
-            name: 'SelectedOption',
-            description: 'Select an option',
-            defaultValue: '',
-            // Use Groovy script to generate options dynamically
-            // This will create a dropdown with dynamic options
-            // based on the result of myMethod()
-            choices: getOptions()
-        )
-    }
 
     stages {
         stage('Example') {
@@ -21,21 +40,4 @@ pipeline {
             }
         }
     }
-}
-
-def getOptions() {
-    def options = []
-    // Call your method to generate options
-    def methodResult = myMethod()
-    // Assuming myMethod() returns a list of strings
-    methodResult.each { option ->
-        options.add(option)
-    }
-    return options.join('\n')
-}
-
-def myMethod() {
-    // Your method implementation here
-    // Should return a list of strings representing the options
-    return ['Option1', 'Option2', 'Option3']
 }
